15e0c33de186d7c31469e4bf3629f10288ec654f
mathvra <matheusvram@gmail.com>
2024-01-25 00:20:54 -0300

    feat: assign button

--- app/page.tsx
+++ app/page.tsx
@@ -8,15 +8,49 @@ const GET_PRODUCTS = gql`
       name
       description
       url
+      _id
+      isAssigned
     }
   }
 `;
 
+const token = process.env.NEXT_PUBLIC_SANITY_TOKEN;
+const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+
 export default function Home() {
-  const { data, loading } = useQuery(GET_PRODUCTS);
+  const { data, loading, refetch } = useQuery(GET_PRODUCTS);
 
   if (loading) {
     return <div>Carregando...</div>;
+  }
+
+  function handleClick(id: string, isAssigned: boolean) {
+    console.log(id, isAssigned);
+    const mutations = [
+      {
+        patch: {
+          id: id,
+          set: {
+            isAssigned: !isAssigned,
+          },
+        },
+      },
+    ];
+
+    fetch(
+      `https://${projectId}.api.sanity.io/v2023-08-01/data/mutate/production`,
+      {
+        method: "post",
+        headers: {
+          "Content-type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({ mutations }),
+      }
+    )
+      .then((response) => response.json())
+      .then(() => refetch())
+      .catch((error) => console.error(error));
   }
 
   return (
@@ -29,7 +63,15 @@ export default function Home() {
             name,
             description,
             url,
-          }: { name: string; description: string; url: string },
+            _id,
+            isAssigned,
+          }: {
+            name: string;
+            description: string;
+            url: string;
+            _id: string;
+            isAssigned: boolean;
+          },
           index: number
         ) => {
           return (
@@ -37,6 +79,10 @@ export default function Home() {
               <h3>{name}</h3>
               <p>{description}</p>
               <Link href={url}>Sugestão de presente</Link>
+              <br />
+              <button onClick={() => handleClick(_id, isAssigned)}>
+                Assinar
+              </button>
               <br />
               ---------------------
             </div>
