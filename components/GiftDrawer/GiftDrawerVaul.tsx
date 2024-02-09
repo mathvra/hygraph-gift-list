d0bbd968a99a8f211ab82c5238427133e7247f7f
mathvra <matheusvram@gmail.com>
2024-02-09 13:47:42 -0300

    fix: drawer

--- components/GiftDrawer/GiftDrawerVaul.tsx
+++ components/GiftDrawer/GiftDrawerVaul.tsx
@@ -46,84 +46,86 @@ export default function GiftDrawer({
       </Drawer.Trigger>
       <Drawer.Portal>
         <Drawer.Overlay className="fixed inset-0 bg-black/40" />
-        <Drawer.Content className="bg-zinc-100 flex flex-col rounded-t-lg fixed bottom-0 left-0 right-0 p-4 gap-4 h-fit max-h-fit overflow-auto after:!content-none">
-          <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300" />
-          {!isAssigned && (
-            <GiftForm
-              isAssigned={isAssigned}
-              id={id}
-              refetch={refetch}
-              setSubmitLoading={setSubmitLoading}
-            />
-          )}
-          <Drawer.Title>{name}</Drawer.Title>
-          <Drawer.Description>{description}</Drawer.Description>
-          {!isAssigned && (
-            <Button asChild>
-              <Link href={url} target="_blank">
-                <ArrowSquareOut weight="bold" />
-                Ver na loja
-              </Link>
-            </Button>
-          )}
-          <div>
-            {isAssigned ? (
-              <div>
-                <span className="text-secondary-0 font-bold">
-                  Esse presente já foi assinado!
-                </span>
-                <p className="text-secondary-0 text-sm">
-                  Não se preocupe, você pode escolher outro presente que esteja
-                  disponível na lista!
-                </p>
-              </div>
-            ) : (
-              <div>
-                <span className="text-primary-0 font-bold">
-                  Assinar presente:
-                </span>
-                <p className="text-sm">
-                  Preencha suas informações para garantir a sua assinatura do
-                  presente
-                </p>
-              </div>
-            )}
-          </div>
-          <div className="flex gap-4">
-            <Drawer.Close asChild>
-              {isAssigned ? (
-                <Button variant="secondaryWhite" className="w-1/2">
-                  Voltar
-                </Button>
-              ) : (
-                <Button variant="defaultWhite" className="w-1/2">
-                  Cancelar
-                </Button>
-              )}
-            </Drawer.Close>
-            {isAssigned ? (
-              <Button variant="secondary" className="w-1/2" asChild>
+        <Drawer.Content className="bg-zinc-100 flex flex-col rounded-t-lg fixed bottom-0 left-0 right-0">
+          <div className="mx-auto w-12 h-1.5 flex-shrink-0 rounded-full bg-zinc-300 mt-4" />
+          <div className="flex flex-col overflow-auto p-4 gap-4">
+            <Drawer.Title>{name}</Drawer.Title>
+            <Drawer.Description>{description}</Drawer.Description>
+            {!isAssigned && (
+              <Button asChild>
                 <Link href={url} target="_blank">
                   <ArrowSquareOut weight="bold" />
                   Ver na loja
                 </Link>
               </Button>
-            ) : submitLoading ? (
-              <Button className="w-1/2" type="submit" form="gift-form">
-                <CircleNotch weight="bold" className="animate-spin" />
-                Carregando
-              </Button>
-            ) : (
-              <Button
-                variant="defaultStrong"
-                className="w-1/2"
-                type="submit"
-                form="gift-form"
-              >
-                <PencilSimpleLine weight="bold" />
-                Assinar
-              </Button>
             )}
+            <div>
+              {isAssigned ? (
+                <div>
+                  <span className="text-secondary-0 font-bold">
+                    Esse presente já foi assinado!
+                  </span>
+                  <p className="text-secondary-0 text-sm">
+                    Não se preocupe, você pode escolher outro presente que
+                    esteja disponível na lista!
+                  </p>
+                </div>
+              ) : (
+                <div>
+                  <span className="text-primary-0 font-bold">
+                    Assinar presente:
+                  </span>
+                  <p className="text-sm">
+                    Preencha suas informações para garantir a sua assinatura do
+                    presente
+                  </p>
+                </div>
+              )}
+            </div>
+            {!isAssigned && (
+              <GiftForm
+                isAssigned={isAssigned}
+                id={id}
+                refetch={refetch}
+                setSubmitLoading={setSubmitLoading}
+              />
+            )}
+            <div className="flex gap-4">
+              <Drawer.Close asChild>
+                {isAssigned ? (
+                  <Button variant="secondaryWhite" className="w-1/2">
+                    Voltar
+                  </Button>
+                ) : (
+                  <Button variant="defaultWhite" className="w-1/2">
+                    Cancelar
+                  </Button>
+                )}
+              </Drawer.Close>
+              {isAssigned ? (
+                <Button variant="secondary" className="w-1/2" asChild>
+                  <Link href={url} target="_blank">
+                    <ArrowSquareOut weight="bold" />
+                    Ver na loja
+                  </Link>
+                </Button>
+              ) : submitLoading ? (
+                <Button className="w-1/2" type="submit" form="gift-form">
+                  <CircleNotch weight="bold" className="animate-spin" />
+                  Carregando
+                </Button>
+              ) : (
+                <Button
+                  variant="defaultStrong"
+                  className="w-1/2"
+                  type="submit"
+                  form="gift-form"
+                >
+                  <PencilSimpleLine weight="bold" />
+                  Assinar
+                </Button>
+              )}
+            </div>
           </div>
         </Drawer.Content>
       </Drawer.Portal>
